ad6afa5a9f9295960f45230172b62e8d4ecd579d
Ehsan Kashfi <ehsanmody04@gmail.com>
2021-01-09 13:32:44 +0330

    + Fixed show and verify payment middleware route

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::resource('payments', \App\Http\Controllers\PaymentController::class)
+    ->only(['show']);
+Route::get('verify', \App\Http\Controllers\VerifyController::class)->name('verify');
+
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
 
     Route::get('dashboard', [\App\Http\Controllers\HomeController::class, 'index'])->name('dashboard');
 
-    Route::resource('payments', \App\Http\Controllers\PaymentController::class);
-    Route::get('verify', \App\Http\Controllers\VerifyController::class)->name('verify');
+    Route::resource('payments', \App\Http\Controllers\PaymentController::class)
+        ->except(['show']);
 
     Route::resource('tags', \App\Http\Controllers\TagController::class);
 });
