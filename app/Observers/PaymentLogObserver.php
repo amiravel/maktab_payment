b9351b4b3990c9da997ee40d99a92a9b0e762d55
Ehsan Kashfi <ehsanmody04@gmail.com>
2021-01-30 12:37:19 +0330

    + Added only send SMS to specific tags

--- app/Observers/PaymentLogObserver.php
+++ app/Observers/PaymentLogObserver.php
@@ -4,19 +4,20 @@ namespace App\Observers;
 
 use App\Models\PaymentLog;
 use App\Notifications\InvoicePaid;
-use Illuminate\Support\Facades\Notification;
 
 class PaymentLogObserver
 {
     /**
      * Handle the PaymentLog "created" event.
      *
-     * @param  \App\Models\PaymentLog  $paymentLog
+     * @param \App\Models\PaymentLog $paymentLog
      * @return void
      */
     public function created(PaymentLog $paymentLog)
     {
-        if ($paymentLog->payment->status('successful'))
-            $paymentLog->notify(new InvoicePaid($paymentLog));
+        // TODO: changed to dynamic tags id
+        if (count(array_diff([134, 131], $paymentLog->payment->tags->pluck('id')->toArray())) < 2)
+            if ($paymentLog->payment->status('successful'))
+                $paymentLog->notify(new InvoicePaid($paymentLog));
     }
 }
